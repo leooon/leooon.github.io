e536feee599ee2040378dd88c04097c2627791e4
Leon Santiago <leonnaoexiste@gmail.com>
2021-04-23 18:38:20 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -1,3 +1,24 @@
+let golpes = [];
+
+class Golpe {
+	constructor(nome, energia, respeito) {
+		this.nome     = nome;
+		this.energia  = energia;
+		this.respeito = respeito;
+
+		golpes.push(this);
+	}
+
+	greet() {
+		return `${this.name} says hello.`;
+	}
+}
+new Golpe('Air Flare', 30, 45);
+new Golpe('Deadman', 75, 120);
+new Golpe('Baby Freeza', 110, 180);
+new Golpe('Hellicopter', 30, 80);
+new Golpe('The Frog', 140, 400);
+
 function sel(t) {
 	return document.querySelectorAll(t);
 }
@@ -5,21 +26,39 @@ function rand(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let timer;
+function montarMenuGolpes() {
+	const menu = sel('#menuGolpes')[0];
+
+	golpes.forEach((golpe) => {
+		let botao = `
+		<div class="botao" data-energia="${golpe.energia}" data-respeito="${golpe.respeito}" data-nome="${golpe.nome}">
+			<div class="nome">${golpe.nome}</div>
+			<div class="energia-valor">${golpe.energia}e</div>
+			<div class="respeito-valor">${golpe.respeito}r</div>
+		</div>
+		`;
+		
+		menu.insertAdjacentHTML('afterbegin', botao);
+	});
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+	ativaMenu();
+	montarMenuGolpes();
 	timer = setInterval(iniciarTurno, 1000);
 });
 
-let energiaTotal = 200;
-let energia      = 200;
-let energiaGanho = 100;
-let contagem     = 3;
+let energiaTotal    = 200;
+let energia         = 200;
+let energiaGanho    = 100;
+let contagem        = 3;
+let respeitoVoce    = 0;
+let respeitoInimigo = 0;
 
 function escolheGolpes() {
 	let contagemAtual = contagem;
 
-	let golpes = sel('#menu .botao');
+	let golpes = sel('#menuGolpes .botao');
 	golpes = [].slice.call(golpes);
 
 	while (contagemAtual) {
@@ -30,6 +69,7 @@ function escolheGolpes() {
 			golpes[escolhido].classList.add('ativo');
 
 			criaLog('Usou '+ golpes[escolhido].dataset.nome +'. -'+ golpes[escolhido].dataset.energia +' energia.<br>');
+			respeitoVoce += parseInt(golpes[escolhido].dataset.respeito);
 		} else {
 			golpes[escolhido].classList.add('proibido');
 
@@ -61,17 +101,17 @@ function iniciarTurno() {
 
 	escolheGolpes();
 	atualizaEnergia();
+	atualizaRespeito();
 
-	console.log("Turno: "+ turno);
-	console.log("Energia: "+ energia);
-	console.log("-----");
 	turno++;
+	let infoTurno = sel('#turno')[0];
+	infoTurno.innerHTML = turno;
 
 	if (turno == 5) clearInterval(timer);
 }
 
 function resetBotoes() {
-	let golpes = sel('#menu .botao');
+	let golpes = sel('#menuGolpes .botao');
 	golpes.forEach(function(el) {
 		el.classList.remove('ativo', 'proibido');
 	});
@@ -80,7 +120,21 @@ function atualizaEnergia() {
 	porcentagem = energia / 2;
 	sel('#preenchimento')[0].style.width = porcentagem +'%';
 }
+function atualizaRespeito() {
+	sel('#voce-respeito')[0].innerHTML = respeitoVoce;
+}
 function criaLog(texto) {
 	let log = sel('#log')[0];
 	log.insertAdjacentHTML('afterbegin', texto);
+}
+
+function ativaMenu() {
+	let botoesMenu = sel('#menu div');
+
+	botoesMenu.forEach(el => el.addEventListener('click', () => {
+		let ativo = sel('#menu div.ativo')[0];
+		ativo.classList.remove('ativo');
+
+		el.classList.add('ativo');
+	}));
 }
