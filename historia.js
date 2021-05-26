2ddc36b87cf9e6f363ce2d5f9abb07e9b6434367
Leon Santiago <leonnaoexiste@gmail.com>
2021-05-25 22:57:01 -0300

    Add files via upload

--- historia.js
+++ historia.js
@@ -1,18 +1,48 @@
 let paginaAtual = 0;
 let capitulos = [
-	{texto: `
-		Seu Teodoro está na praça comendo um churros. Pela quarta vez essa semana, a pomba passa rasteira deixando seu presente na testa de seu Teodoro.<br><br>
-		Já chega, pensa seu Teodoro.<br><br>
-		Hora de destruir o mundo.
-	`},
-	{texto: `
-		Tomando seu sorvete, você observa e sente o perigo.<br><br>
-		É chegada a hora de ajudar o próximo, talvez você seja o escolhido. O peso da humanidade recai em suas costas.
-	`},
+	{
+		texto: `
+			Seu Teodoro está na praça comendo um churros. Pela quarta vez essa semana, a pomba passa rasteira deixando seu presente na testa de seu Teodoro.<br><br>
+			Já chega, pensa seu Teodoro.<br><br>
+			Hora de destruir o mundo.
+		`,
+		antes: function() {
+
+		},
+		depois: function() {
+			paginaAtual++
+			page.addEventListener('click', proximaPagina)
+		}
+	},
+	{
+		texto: `
+			Tomando seu sorvete, você observa e sente o perigo.<br><br>
+			É chegada a hora de ajudar o próximo.<br>
+			Fazer a diferença.<br>
+			Você pode ser o escolhido.<br><br>
+			O peso da humanidade recai em suas costas.
+		`,
+		antes: function() {
+			let html = `<div class="botao" id="terminarSorvete">terminar seu sorvete</div>`
+			cena.insertAdjacentHTML('beforeend', html)
+		},
+		depois: function() {
+			page.addEventListener('click', this.mostraBotao, {once: true})
+		},
+		mostraBotao: function() {
+			let sorvete = sel('#terminarSorvete')[0]
+			sorvete.classList.add('ativo')
+			sorvete.addEventListener('click', capitulos[paginaAtual].vaiParaAcademia, {once: true})
+		},
+		vaiParaAcademia: function() {
+			sel('#menu div.batalha')[0].click();
+		}
+	},
 ];
 
+let cena;
 function montaCenaHistoria() {
-	let cena = sel('#cena')[0]
+	cena = sel('#cena')[0]
 	cena.innerHTML = ""
 	cena.classList.remove(...cena.classList)
 	cena.classList.add('historia')
@@ -30,7 +60,6 @@ function entrarCapitulo() {
 	let page = sel('#cena.historia #page')[0]
 
 	page.removeEventListener('click', entrarCapitulo)
-	// page.addEventListener('click', proximaPagina)
 
 	let cena = sel('#cena')[0];
 	
@@ -42,16 +71,17 @@ function entrarCapitulo() {
 
 function proximaPagina() {
 	let page = sel('#cena.historia #page')[0]
-	sel('#cena.historia #page')[0].innerHTML = ""
+	
+	page.removeEventListener('click', proximaPagina)
+	page.innerHTML = ""
+
+	capitulos[paginaAtual].antes()
 	
 	let texto = `<div class="texto">${capitulos[paginaAtual].texto}</div>`
 	page.insertAdjacentHTML('beforeend', texto)
 
-	animar();
-
-	paginaAtual++
+	animar()
 }
-
 
 function animar() {
 	let texto = sel('#cena.historia .texto')[0]
@@ -61,7 +91,11 @@ function animar() {
 	function avancarTexto() {
 		texto.innerHTML = base.substring(0, limite) + `<span>${base.substring(limite)}</span>`
 		
-		if (limite == base.length) clearInterval(animacao)
+		if (limite == base.length) {
+			clearInterval(animacao)
+			page.removeEventListener('click', avancarTudo)
+			capitulos[paginaAtual].depois()
+		} 
 
 		if (base[limite] == '<') {
 			limite += 4
@@ -79,4 +113,5 @@ function animar() {
 	page.addEventListener('click', avancarTudo)
 
 	let animacao = setInterval(avancarTexto, 25)
+	avancarTexto()
 }
